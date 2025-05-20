ce5d6d63c378ad0b4afe791eb171383ff16fd5e9
uc02 <upendrachauhan0206@gmail.com>
2025-05-20 22:18:33 +0530

    completed routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { protectRoute } from '../middleware/auth.middleware';
-import { getRecommededUsers, getMyFriends } from '../controllers/user.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getRecommededUsers, getMyFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests, getOutgoingFriendReqs } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
@@ -9,5 +9,12 @@ router.use(protectRoute)
 
 router.get('/', getRecommededUsers)
 router.get('/friends', getMyFriends)
+
+router.post('/friend-request/:id', sendFriendRequest); 
+router.put('/friend-request/:id/accept', acceptFriendRequest); 
+
+router.get("/friend-requests", getFriendRequests)
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs)
+
 
 export default router;
