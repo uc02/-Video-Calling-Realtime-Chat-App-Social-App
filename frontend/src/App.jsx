db2d819d2dfbd91e02aa906da2e5f599dc1551b1
uc02 <upendrachauhan0206@gmail.com>
2025-05-21 13:49:31 +0530

    add: signuppage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import Notification from './pages/NotificationsPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
+
+export default function App() {
+  
+  const { data:authData, isLoading, error } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data
+    },
+    retry:false,
+  })
+   const authUser = authData?.user
+  return (
+    <div className="h-screen " data-theme="night">
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/notification" element={authUser ? <Notification /> : <Navigate to="/login" />} />
+        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  )
+}
